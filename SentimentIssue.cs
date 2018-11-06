afbc8ad9ada9ef4a2e4fdc634aa79461a8cab14d
saibimajdi <saibimajdi@outlook.com>
2018-11-06 10:53:32 -0700

    first

---
+++ SentimentIssue.cs
@@ -0,0 +1,10 @@
+using Microsoft.ML.Runtime.Api;
+
+namespace ML_AI
+{
+    public class SentimentIssue
+    {
+        public bool Label { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ SentimentPrediction.cs
@@ -0,0 +1,16 @@
+using Microsoft.ML.Runtime.Api;
+
+namespace ML_AI
+{
+    class SentimentPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        [ColumnName("Probability")]
+        public float Probability { get; set; }
+
+        [ColumnName("Score")]
+        public float Score { get; set; }
+    }
+}
